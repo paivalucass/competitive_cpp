3a80a87017cb81551c596f4ed306737734d67ed9
paivalucass <lucaspaiva2805@gmail.com>
2025-11-07 15:46:46 -0300

    update lib

--- lib.cpp
+++ lib.cpp
@@ -195,6 +195,29 @@ void bfs(int start, vector<vector<int>>& adj) {
     }
 }
 
+int binarySearch(vector<int> &arr, int x) {
+    int low = 0;
+    int high = arr.size() - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // Check if x is present at mid
+        if (arr[mid] == x)
+            return mid;
+
+        // If x greater, ignore left half
+        if (arr[mid] < x)
+            low = mid + 1;
+
+        // If x is smaller, ignore right half
+        else
+            high = mid - 1;
+    }
+
+    // If we reach here, then element was not present
+    return -1;
+}
+
 // FENWICK TREE USAGE
 
 int main() {
